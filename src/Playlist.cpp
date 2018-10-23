18d8f941196b86f5abfce8df3ff629794b9727e8
Alexander "Ace" Olofsson <ace@haxalot.com>
2018-10-23 20:01:44 +0200

    Start stubbing out playlists

---
+++ src/Playlist.cpp
@@ -0,0 +1,117 @@
+#include "Playlist.hpp"
+#include "Util/Path.hpp"
+// #include "Util/WorkQueue.hpp"
+
+#include <algorithm>
+#include <chrono>
+
+// TODO: Place somewhere more reasonable
+// Util::WorkQueue s_songUpdateQueue;
+
+Playlist::Playlist()
+{
+}
+
+Playlist::~Playlist()
+{
+}
+
+Playlist::SongArray::const_iterator Playlist::cbegin() const
+{
+    return std::cbegin(m_songs);
+}
+Playlist::SongArray::const_iterator Playlist::cend() const
+{
+    return std::cbegin(m_songs);
+}
+Playlist::SongArray::iterator Playlist::begin()
+{
+    return std::begin(m_songs);
+}
+Playlist::SongArray::iterator Playlist::end()
+{
+    return std::end(m_songs);
+}
+size_t Playlist::size() const
+{
+    return m_songs.size();
+}
+
+bool Playlist::hasSong(const std::string& aSearch) const
+{
+    return std::find_if(cbegin(), cend(), [aSearch](auto& it) {
+        return it.URL == aSearch || it.Title == aSearch || it.StreamURL == aSearch;
+    }) != cend();
+}
+void Playlist::addSong(const std::string& aUrl)
+{
+    m_songs.push_back({ aUrl });
+}
+void Playlist::removeSong(const std::string& aSearch)
+{
+    auto it = std::find_if(cbegin(), cend(), [aSearch](auto& it) {
+        return it.URL == aSearch || it.Title == aSearch || it.StreamURL == aSearch;
+    });
+
+    if (it != cend())
+        m_songs.erase(it);
+}
+void Playlist::removeSong(size_t aSong)
+{
+    if (aSong < m_songs.size())
+        m_songs.erase(cbegin() + aSong);
+}
+void Playlist::removeAllSongs()
+{
+    m_songs.clear();
+}
+
+void Playlist::update()
+{
+    auto now = std::chrono::system_clock::now().time_since_epoch();
+    auto updateClock = std::chrono::duration_cast<std::chrono::seconds>(now).count();
+
+    for (auto& it : m_songs)
+    {
+        if (it.UpdateTime > updateClock)
+            continue;
+
+        // TODO
+        // s_songUpdateQueue.queueTask<void>([]() { });
+
+        it.UpdateTime = updateClock + 3600;
+    }
+}
+
+void Playlist::addFromPlaylist(const Playlist& aPlaylist)
+{
+    m_songs.resize(m_songs.size() + aPlaylist.size());
+    for (const auto& s : aPlaylist.m_songs)
+        m_songs.push_back(s);
+}
+
+bool Playlist::addFromFile(const std::string& aPath)
+{
+    auto path = Util::ExpandPath(aPath);
+
+    // TODO
+
+    return false;
+}
+
+bool Playlist::loadFromFile(const std::string& aPath)
+{
+    auto path = Util::ExpandPath(aPath);
+
+    // TODO
+
+    return false;
+}
+bool Playlist::saveToFile(const std::string& aPath) const
+{
+    auto path = Util::ExpandPath(aPath);
+
+    // TODO
+
+    return false;
+}
--- src/Playlist.hpp
+++ src/Playlist.hpp
@@ -1,28 +1,76 @@
 #pragma once
 
+#include <vector>
 #include <string>
 
 class Playlist
 {
 public:
+    struct Song
+    {
+        std::string URL;
+
+        std::string Title;
+        std::string StreamURL;
+        std::string ThumbnailURL;
+
+        uint32_t UpdateTime;
+    };
+
+    using SongArray = std::vector<Song>;
+
     Playlist();
     Playlist(const Playlist& copy) = default;
     Playlist(Playlist&& move) = default;
-    ~Playlist();
+    virtual ~Playlist();
 
     Playlist& operator=(const Playlist& copy) = default;
     Playlist& operator=(Playlist&& move) = default;
 
-    bool hasSong(const std::string& aUrl) const;
-    bool addSong(const std::string& aUrl);
-    bool removeSong(const std::string& aUrl);
-    bool removeSong(size_t aSong);
-    bool removeAllSongs();
+    SongArray::const_iterator cbegin() const;
+    SongArray::const_iterator cend() const;
+    SongArray::iterator begin();
+    SongArray::iterator end();
+    size_t size() const;
+
+    bool hasSong(const std::string& aSearch) const;
+    void addSong(const std::string& aUrl);
+    void removeSong(const std::string& aSearch);
+    void removeSong(size_t aSong);
+    void removeAllSongs();
+
+    virtual void update();
+
+    void addFromPlaylist(const Playlist& aPlaylist);
+    bool addFromFile(const std::string& aPath);
+    bool loadFromFile(const std::string& aPath);
+    bool saveToFile(const std::string& aPath) const;
+
+private:
+    SongArray m_songs;
+};
+
+enum PlayFlags : uint8_t
+{
+    PF_Random  = 1u << 0u,
+    PF_Consume = 1u << 1u,
+    PF_Single  = 1u << 2u,
+};
+
+class ActivePlaylist : public Playlist
+{
+public:
+    ActivePlaylist();
 
     void update();
 
-    void loadFromFile(const std::string& aPath);
-    void saveToFile(const std::string& aPath) const;
+    bool hasRandom() const;
+    void setRandom(bool aRandom = true);
+    bool hasConsume() const;
+    void setConsume(bool aConsume = true);
+    bool hasSingle() const;
+    void setSingle(bool aSingle = true);
 
 private:
+    PlayFlags m_playFlags;
 };
