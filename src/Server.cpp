719377654958b8bc7c594d17c984f8ac0627b96b
Alexander "Ace" Olofsson <ace@haxalot.com>
2019-03-12 23:31:17 +0100

    Move logging init to main instead of server

--- src/Server.cpp
+++ src/Server.cpp
@@ -12,36 +12,8 @@
 
 using namespace std::literals::chrono_literals;
 
-namespace
-{
-
-std::string formatTime(Util::LogLevels aLevel)
-{
-    auto now = std::chrono::system_clock::now();
-    auto time = std::chrono::system_clock::to_time_t(now);
-    auto ts = localtime(&time);
-    char timeBuf[32];
-    int len = std::strftime(timeBuf, 32, "%H:%M:%S|  ", ts);
-
-    static constexpr char LogLevels[] = { 'D', 'I', 'W', 'E' };
-    timeBuf[len-2] = LogLevels[int(aLevel)];
-    return timeBuf;
-}
-
-}
-
 Server::Server()
 {
-    auto outputLogger = new Util::StdoutLogger;
-    auto combinedLogger = new Util::CombinedLogger;
-    auto timeLogger = new Util::PrependLogger(combinedLogger);
-
-    combinedLogger->addLogger(outputLogger);
-    timeLogger->setPrepend(formatTime);
-
-    Util::SetLogger(timeLogger);
-    Util::SetLogLevel(Util::Log_Debug);
-
     m_config.loadDefaults();
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,36 @@
 #include "Server.hpp"
+#include "Util/Logging.hpp"
+
+namespace
+{
+
+std::string formatTime(Util::LogLevels aLevel)
+{
+    auto now = std::chrono::system_clock::now();
+    auto time = std::chrono::system_clock::to_time_t(now);
+    auto ts = localtime(&time);
+    char timeBuf[32];
+    int len = std::strftime(timeBuf, 32, "%H:%M:%S|  ", ts);
+
+    static constexpr char LogLevels[] = { 'D', 'I', 'W', 'E' };
+    timeBuf[len-2] = LogLevels[int(aLevel)];
+    return timeBuf;
+}
+
+}
 
 int main(int argc, const char** argv)
 {
+    auto outputLogger = new Util::StdoutLogger;
+    auto combinedLogger = new Util::CombinedLogger;
+    auto timeLogger = new Util::PrependLogger(combinedLogger);
+
+    combinedLogger->addLogger(outputLogger);
+    timeLogger->setPrepend(formatTime);
+
+    Util::SetLogger(timeLogger);
+    Util::SetLogLevel(Util::Log_Debug);
+
     Server srv;
 
     srv.init(argc, argv);
